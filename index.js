e45551b3841a099710dcd893731b3885c3887216
Matt Zuzolo <mattzuzolo@outlook.com>
2018-02-23 20:00:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,9 +78,11 @@ function createRock(x) {
             // Hmmm, why would we have used `var` here?
   var top = 0
   
+   rock.style.top = `${top += 2}px`
+  
   function step () {
     
-     rock.style.top = `${top += 2}px`
+    
      
      if (top < 200) {
        window.requestAnimationFrame(step);
