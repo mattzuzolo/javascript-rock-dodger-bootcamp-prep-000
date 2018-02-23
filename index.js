032bc1d76130b591408d5ad8637831d391ffcc84
Matt Zuzolo <mattzuzolo@outlook.com>
2018-02-23 20:38:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,6 +78,17 @@ function createRock(x) {
             // Hmmm, why would we have used `var` here?
   var top = 0
   
+  function step () {
+    
+     
+     
+     if (top < 200) {
+       window.requestAnimationFrame(step);
+     }
+  }
+    window.requestAnimationFrame(step);
+             
+            
               /**
                * Now that we have a rock, we'll need to append
                * it to GAME and move it downwards.
